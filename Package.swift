32e77db9b3bde5e3e64ccde61c212fe687880a10
Michał Gutowski <mgutski@icloud.com>
2025-07-12 15:15:28 +0200

    Update package dependencies to latest versions

--- Package.swift
+++ Package.swift
@@ -43,10 +43,10 @@ var package = Package(
 
 #if os(macOS)
 package.dependencies.append(contentsOf: [
-    .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.5.0"),
+    .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.6.1"),
     .package(url: "https://github.com/swiftlang/swift-syntax.git", "509.1.1"..<"602.0.0"),
     .package(url: "https://github.com/pointfreeco/swift-parsing.git", from: "0.14.1"),
-    .package(url: "https://github.com/jpsim/Yams.git", from: "5.3.1")
+    .package(url: "https://github.com/jpsim/Yams.git", from: "5.4.0")
 ])
 package.targets.append(contentsOf: [
     // Core Module
